c56cd121f8cc65c1f8cfdc178d6ffa531cb28d71
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-07-20 23:02:40 +0530

    Update fact.sh

--- fact.sh
+++ fact.sh
@@ -10,3 +10,21 @@ do
 done
 
 echo $fact
+
+
+
+
+
+----------------------------------------------
+echo "enter the number"
+read n
+f=1
+for((i=1;i<=n;i++))
+do
+f=$((f * i))
+done
+echo "fact:$f"
+
+
+
+
